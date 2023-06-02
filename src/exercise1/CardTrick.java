2072cd3c9e5d4319bcf5363c635b8d946c689523
Dhavalbhuva16 <134092379+Dhavalbhuva16@users.noreply.github.com>
2023-06-02 11:39:32 -0400

    Update CardTrick.java

--- src/exercise1/CardTrick.java
+++ src/exercise1/CardTrick.java
@@ -45,19 +45,19 @@ public class CardTrick {
         System.out.println("Congratulations, you guessed right!");
         System.out.println();
 
-        System.out.println("My name is DHAVAL, but you can call me DHAVAL or sir");
+        System.out.println("My name is DHAVAL, but you can call me DS");
         System.out.println();
 
         System.out.println("My career ambitions:");
-        System.out.println("-- Be more active on LinkedIn");
+        System.out.println("-- Gain more programming skils");
         System.out.println("-- Have a semester with no violations of academic integrity!");
         System.out.println();
 
         System.out.println("My hobbies:");
         System.out.println("-- Investing");
         System.out.println("-- Cooking");
-        System.out.println("-- Reading/Watching TV");
-        System.out.println("-- Riding my motorcycle");
+        System.out.println("-- Playing cricket");
+        System.out.println("-- Excercise");
 
         System.out.println();
     }
